aeb89d161326648ab8c78f2e84cfd9943f349256
Helena Hedström <31826611+helenahedstrom@users.noreply.github.com>
2018-11-07 14:41:49 +0100

    Added classes and repo

---
+++ HelenaBank/Repo/BankRepository.cs
@@ -0,0 +1,67 @@
+﻿using HelenaBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelenaBank.Repo
+{
+    public class BankRepository
+    {
+        
+        public List<Customer> ImportCustomers()
+        {
+            List<Customer> listOfCustomers = new List<Customer>();
+            var reader = new StreamReader(@"D:\Helena\Documents\Prog17\ALM\HelenaBank\HelenaBank\Files\bankdata-small.txt");
+
+            int Customers = int.Parse(reader.ReadLine());
+
+            for (int i = 0; i < Customers; i++)
+            {
+                string cust = reader.ReadLine();
+                string[] arrayOfCustomer = cust.Split(';');
+                var customer = new Customer()
+                {
+                    Id = arrayOfCustomer[0],
+                    OrgNumber = arrayOfCustomer[1],
+                    Name = arrayOfCustomer[2],
+                    Address = arrayOfCustomer[3],
+                    City = arrayOfCustomer[4],
+                    State = arrayOfCustomer[5],
+                    ZipCode = arrayOfCustomer[6],
+                    Country = arrayOfCustomer[7],
+                    Phone = arrayOfCustomer[8]
+                };
+                listOfCustomers.Add(customer);
+            }
+
+            var numberOfAccounts = int.Parse(reader.ReadLine());
+
+            for (int i = 0; i < numberOfAccounts; i++)
+            {
+                string acc = reader.ReadLine();
+                string[] arrayOfAccount = acc.Split(';');
+
+                var account = new Account()
+                {
+                    AccountNo = arrayOfAccount[0],
+                    CustomerId = arrayOfAccount[1],
+                    Balance = decimal.Parse(arrayOfAccount[2], CultureInfo.InvariantCulture)
+            };
+
+                foreach (var cust in listOfCustomers)
+                {
+                    if (cust.Id == account.CustomerId)
+                    {
+                        cust.Accounts.Add(account);
+                    }
+                }
+                
+            }
+
+            return listOfCustomers;
+        }
+    }
+}
